78c6a5a1335a124bb1357a14666af3d93c86b1c8
Joey2Jobs <scholz.joe@gmail.com>
2019-03-02 21:41:55 -0600

    some question html, 1 friends eample

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friendsArray = [
+	{
+		name: 'Freddy',
+		photo: 'https://cakeduptees.files.wordpress.com/2015/04/freddy-krueger.png',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
