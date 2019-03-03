a154a82f22e9ebd150b8054a9bc80a0fd53e13bb
Joey2Jobs <scholz.joe@gmail.com>
2019-03-02 21:16:32 -0600

    simple html, routes but no post

---
+++ server.js
@@ -0,0 +1,30 @@
+// DEPENDENCIES
+var express = require("express");
+var path = require("path");
+
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("app/public"));
+
+// Routes
+// =============================================================
+require("./app/routes/apiRoutes.js")(app);
+require("./app/routes/htmlRoutes.js")(app);
+
+
+// LISTENER
+// The below code effectively "starts" our server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
